975b5583d412b06ae36b0e4c1e4084b9b83920c1
Ryuhei Yokokawa <ryuhei@y-designs.com>
2015-05-18 22:49:24 +0000

    got caching to work.

--- admin.php
+++ admin.php
@@ -15,46 +15,48 @@ function yd_bh_create_menu() {
 
 function register_bhrss_settings() {
 	//register our settings
-	register_setting( 'bhrss-settings-group', 'rss_url' );
-	register_setting( 'bhrss-settings-group', 'load_styling' );
-	register_setting( 'bhrss-settings-group', 'cache' );
+	register_setting( 'bhrss-settings-group', 'bh_rss_url' );
+	register_setting( 'bhrss-settings-group', 'bh_load_styling' );
+	register_setting( 'bhrss-settings-group', 'bh_cache' );
 }
 
 function bhrss_settings_page() {
-?>
-<h2>Bullhorn </h2>
+	//yup, classc WP stuff. ?>
 
-<form method="post" action="options.php">
-    <?php settings_fields( 'baw-settings-group' ); ?>
-    <?php do_settings_sections( 'baw-settings-group' ); ?>
-    <table class="form-table">
-        <tr valign="top">
-        	<th scope="row">URL</th>
-	        <td>
-	        	<input type="text" name="rss_url" value="<?php echo esc_attr( get_option('rss_url') ); ?>" />
-	        </td>
-        </tr>
-         
-        <tr valign="top">
-        	<th scope="row">Load Styling?</th>
-	        <td>
-	        	<input type="text" name="load_styling" value="<?php echo esc_attr( get_option('load_styling') ); ?>" />
-	        </td>
-        </tr>
-        
-        <tr valign="top">
-	        <th scope="row">Cache for 10 min?</th>
-	        <td>
-	        	<input type="text" name="cache" value="<?php echo esc_attr( get_option('cache') ); ?>" />
-	        </td>
-        </tr>
-    </table>
-    
-    <?php submit_button(); ?>
+	<div class="wrap">
+	<h2>Bullhorn RSS Feed Puller</h2>
 
-</form>
-</div>
+	<form method="post" action="options.php">
+	    <?php settings_fields( 'bhrss-settings-group' ); ?>
+	    <?php do_settings_sections( 'bhrss-settings-group' ); ?>
+	    <table class="form-table">
+	        <tr valign="top">
+	        	<th scope="row">URL</th>
+		        <td>
+		        	<input type="text" name="bh_rss_url" value="<?php echo esc_attr( get_option('bh_rss_url') ); ?>" />
+		        </td>
+	        </tr>
+	         
+	        <tr valign="top">
+	        	<th scope="row">Load Styling?</th>
+		        <td>
+		        	<input type="checkbox" name="bh_load_styling" <?php echo esc_attr( get_option('bh_load_styling') ) ? 'checked' : ''  ; ?> />
+		        </td>
+	        </tr>
+	        
+	        <tr valign="top">
+		        <th scope="row">Cache for 10 min?</th>
+		        <td>
+		        	<input type="checkbox" name="bh_cache" <?php echo esc_attr( get_option('bh_cache') ) ? 'checked' : ''  ; ?> />
+		        </td>
+	        </tr>
+	    </table>
+	    
+	    <?php submit_button(); ?>
 
-<?php
+	</form>
+	</div>
+
+	<?php
+
 }//end of bhrss_settings_page
-?>
--- plugin.php
+++ plugin.php
@@ -9,26 +9,59 @@ Author URI: http://y-designs.com
 License: GPL
 */
 
-include 'admin.php';//I've put all the admin stuff in the other file.
+//I've put all the admin stuff in the other file.
+require_once(dirname(__FILE__) . '/admin.php');
+
+if(!function_exists('yd_bullhorn')) {
+
+	function yd_bullhorn() {
+		$view = '';
+		$objects = get_transient('yd_bullhorn_cache');
+		
+		if( get_option('bh_cache') && $objects !== false ) {
+			//if the cache exists and it isn't expired (10 minutes or 6000 seconds)
+			//objects is set above, note in PHP a single equal sign sets a variable in an if statement.
+			
+		} else {
+			$objects = yd_bullhorn_get();//grab the RSS.
+			$objects = json_encode($objects);//back to array;
+			if(get_option('bh_cache')) {//if the system is set to do a cache, set it.
+				set_transient('yd_bullhorn_cache',$objects, 600);//600 seconds = 10 minutes
+			} else {
+				delete_transient( 'yd_bullhorn_cache');
+			}
+		}
+
+		$objects = json_decode($objects,true);//back to array;
+
+		if(is_array($objects) && count( $objects['channel']['item'] )) {
+			//Definitely not my idea of awesome code, but it works.
+			foreach($objects['channel']['item'] as $object) {
+				$view .='
+				<div class="item">
+				        <a href="'.$object['link'].'">
+				                <h2>'.$object['title'].'</h2>
+				        </a>
+				        <date>'.$object['pubDate'].'</date>
+				        <div class="content">'.$object['description'].'</div>
+				</div>';
+			}
+			return $view;
+		} else {
+			return '';
+		}
+
+	}
+
+	//Add that shortcode.
+	add_shortcode('bullhorn_rss','yd_bullhorn');
 
 
-
-$context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
-$url = "http://cls6.bullhornstaffing.com/JobBoard/Standard/JobOpportunitiesRSS.cfm?privateLabelID=10415&category=";
-$xml = file_get_contents($url, false, $context);
-$objects = simplexml_load_string($xml);
-
-
-	foreach($objects->channel->item as $object):?>
-	<div class="item">
-	        <a href="<?php echo $object->link ?>">
-	                <h2><?php echo $object->title?></h2>
-	        </a>
-	        <date><?php echo $object->pubDate?></date>
-	        <div class="content">
-	                <?php echo $object->description?>
-	        </div>
-	</div>  
-	<?php endforeach;?>
-
-
+	function yd_bullhorn_get() {
+		$context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
+		$url = get_option('bh_rss_url');
+		$xml = file_get_contents($url, false, $context);
+		$objects = simplexml_load_string($xml);
+		return $objects;
+	}
+}
